1d987a38c8a65f8416b177c0c63c85e472ecdb35
vSxF7 <victorrxavierr7@gmail.com>
2024-09-04 09:14:09 -0300

    Imersão DEV Dia 3 (JS e Modificações visuais)

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Enxaqueca",
+        descricao: "A enxaqueca é uma condição neurológica que se manifesta com dores de cabeça intensas, pulsantes e frequentemente acompanhadas de náuseas, vômitos e sensibilidade à luz e ao som.",
+        link: "https://www.msdmanuals.com/pt-br/casa/dist%C3%BArbios-cerebrais-da-medula-espinal-e-dos-nervos/cefaleias/enxaquecas?autoredirectid=24675"
+    },
+    {
+        titulo: "Febre",
+        descricao: "A febre é uma elevação da temperatura corporal acima do normal, geralmente em resposta a infecções ou inflamações. Pode ser acompanhada de outros sintomas como calafrios, suor e mal-estar.",
+        link: "https://www.minhavida.com.br/saude/temas/febre"
+    },
+    {
+        titulo: "Dor de Cabeça",
+        descricao: "A dor de cabeça é um sintoma comum que pode ter diversas causas, desde tensão muscular até condições mais graves como enxaqueca e tumores cerebrais. A intensidade e o tipo de dor variam de acordo com a causa.",
+        link: "https://www.tuasaude.com/dor-de-cabeca/"
+    }
+]
