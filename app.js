49cd5c29b029be876623b4b23d933fd156ef8aea
vSxF7 <victorrxavierr7@gmail.com>
2024-09-06 11:08:48 -0300

    Imersão DEV Dia 5 (JS e Funcionamento do Pesquisar)

--- app.js
+++ app.js
@@ -2,23 +2,43 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
     // Loga a seção encontrada no console para verificar se está correta
-    console.log(section);
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(campoPesquisa == ""){
+      section.innerHTML = ("<p>Nada foi encontrado</p>")
+      return 0;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
 
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
-
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
     // Itera sobre cada elemento 'dado' no array 'dados'
     for (let dado of dados) {
-      // Constrói o HTML para cada item da pesquisa, formatando os dados do objeto 'dado'
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href="${dado.link}" target="_blank">Saiba mais sobre.</a>
-        </div>
-      `;
+      titulo = dado.titulo.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      tags = dado.tags.toLowerCase();
+
+      if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+          // Constrói o HTML para cada item da pesquisa, formatando os dados do objeto 'dado'
+          resultados += `
+          <div class="item-resultado">
+            <h2>
+              <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Saiba mais sobre.</a>
+          </div>
+        `;
+      }
+    }
+
+    if(!resultados){
+      resultados = "<p>Nada foi encontrado</p>"
     }
 
     // Atribui a string com os resultados ao conteúdo HTML da seção
--- dados.js
+++ dados.js
@@ -2,16 +2,73 @@ let dados = [
     {
         titulo: "Enxaqueca",
         descricao: "A enxaqueca é uma condição neurológica que se manifesta com dores de cabeça intensas, pulsantes e frequentemente acompanhadas de náuseas, vômitos e sensibilidade à luz e ao som.",
-        link: "https://www.msdmanuals.com/pt-br/casa/dist%C3%BArbios-cerebrais-da-medula-espinal-e-dos-nervos/cefaleias/enxaquecas?autoredirectid=24675"
+        link: "https://www.msdmanuals.com/pt-br/casa/dist%C3%BArbios-cerebrais-da-medula-espinal-e-dos-nervos/cefaleias/enxaquecas?autoredirectid=24675",
+        tags: "temperatura cabeça febre"
     },
     {
         titulo: "Febre",
         descricao: "A febre é uma elevação da temperatura corporal acima do normal, geralmente em resposta a infecções ou inflamações. Pode ser acompanhada de outros sintomas como calafrios, suor e mal-estar.",
-        link: "https://www.minhavida.com.br/saude/temas/febre"
+        link: "https://www.minhavida.com.br/saude/temas/febre",
+        tags: "temperatura cabeça calafrio"
     },
     {
         titulo: "Dor de Cabeça",
         descricao: "A dor de cabeça é um sintoma comum que pode ter diversas causas, desde tensão muscular até condições mais graves como enxaqueca e tumores cerebrais. A intensidade e o tipo de dor variam de acordo com a causa.",
-        link: "https://www.tuasaude.com/dor-de-cabeca/"
-    }
+        link: "https://www.tuasaude.com/dor-de-cabeca/",
+        tags: "tensão muscular enxaqueca tumor cerebral"
+    },
+    {
+        "titulo": "Alzheimer",
+        "descricao": "O Alzheimer é uma doença neurodegenerativa progressiva que afeta a memória, o pensamento e o comportamento.",
+        "link": "https://www.minhavida.com.br/saude/temas/alzheimer",
+        "tags": "neurodegeneração memória comportamento"
+      },
+      {
+        "titulo": "Parkinson",
+        "descricao": "A doença de Parkinson é um distúrbio neurodegenerativo que afeta principalmente o movimento, causando tremores, rigidez muscular, lentidão de movimento e instabilidade postural.",
+        "link": "https://www.minhavida.com.br/saude/temas/parkinson",
+        "tags": "neurodegeneração movimento tremor",
+      },
+      {
+        "titulo": "Esclerose Múltipla",
+        "descricao": "A esclerose múltipla é uma doença autoimune crônica que afeta o sistema nervoso central, causando uma ampla variedade de sintomas, como fadiga, problemas de visão, fraqueza muscular e disfunções sensoriais.",
+        "link": "https://www.minhavida.com.br/saude/temas/esclerose-multipla",
+        "tags": "autoimune sistema nervoso central fadiga",
+      },
+      {
+        "titulo": "Epilepsia",
+        "descricao": "A epilepsia é um distúrbio cerebral crônico caracterizado por crises epilépticas recorrentes, que podem se manifestar de diversas formas, como convulsões, perda de consciência ou alterações sensoriais.",
+        "link": "https://www.minhavida.com.br/saude/temas/epilepsia",
+        "tags": "neurologico convulsão consciencia",
+      },
+      {
+        "titulo": "Síndrome do Túnel Carpal",
+        "descricao": "A síndrome do túnel do carpo é uma condição que causa dor, formigamento e fraqueza na mão e no punho, devido à compressão de um nervo no pulso.",
+        "link": "https://www.minhavida.com.br/saude/temas/tun-carpal",
+        "tags": "neurologia dor mão",
+      },
+      {
+        "titulo": "Lúpus Eritematoso Sistêmico",
+        "descricao": "O lúpus é uma doença autoimune crônica que pode afetar múltiplos órgãos e sistemas do corpo, causando fadiga, erupções cutâneas e dores nas articulações.",
+        "link": "https://www.minhavida.com.br/saude/temas/lupus",
+        "tags": "autoimune pele articulações",
+      },
+      {
+        "titulo": "Fibromialgia",
+        "descricao": "A fibromialgia é uma doença crônica caracterizada por dor muscular generalizada, fadiga, distúrbios do sono e sensibilidade a estímulos táteis.",
+        "link": "https://www.minhavida.com.br/saude/temas/fibromialgia",
+        "tags": "dor fadiga sensibilidade",
+      },
+      {
+        "titulo": "Doença de Crohn",
+        "descricao": "A doença de Crohn é uma doença inflamatória intestinal crônica que pode afetar qualquer parte do trato gastrointestinal, causando diarreia, dor abdominal e perda de peso.",
+        "link": "https://www.minhavida.com.br/saude/temas/doenca-de-crohn",
+        "tags": "inflamatoria intestino diarreia",
+      },
+      {
+        "titulo": "Asma",
+        "descricao": "A asma é uma doença inflamatória crônica das vias aéreas que causa episódios recorrentes de chiado, falta de ar, aperto no peito e tosse.",
+        "link": "https://www.minhavida.com.br/saude/temas/asma",
+        "tags": "respiratoria falta de ar tosse",
+      }
 ]
